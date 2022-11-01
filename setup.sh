9dc33a4d3b4c38ab0438f8d81490b94c6ab59901
rashad101 <rah.rony@gmail.com>
2022-11-01 10:02:27 +0100

    fixed setup pipeline

--- setup.sh
+++ setup.sh
@@ -1,5 +1,10 @@
 mkdir data
 python download.py
+wget https://dl.fbaipublicfiles.com/fasttext/vectors-wiki/wiki.simple.zip
+unzip wiki.simple.zip
+mv wiki.simple.bin data/
+rm wiki.simple.vec wiki.simple.zip
+cd ..
 cd saved_model
 unzip grammar.zip
 rm grammar.zip
